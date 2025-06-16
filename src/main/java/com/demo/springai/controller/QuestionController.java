6776aec23c9702da5febfbbded97edf40e675a13
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-06-16 04:39:42 +0300

    add QuestionController

---
+++ src/main/java/com/demo/springai/controller/QuestionController.java
@@ -0,0 +1,28 @@
+package com.demo.springai.controller;
+
+import com.demo.springai.ai.OpenAIService;
+import com.demo.springai.request.GetAnswerRequest;
+import com.demo.springai.response.Answer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/questions")
+public class QuestionController {
+
+    private final OpenAIService openAIService;
+
+    public QuestionController(OpenAIService openAIService) {
+        this.openAIService = openAIService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Answer> getAnswer(@RequestBody GetAnswerRequest getAnswerRequest) {
+        Answer answer = openAIService.getAnswer(getAnswerRequest);
+        return ResponseEntity.ok(answer);
+    }
+
+}
